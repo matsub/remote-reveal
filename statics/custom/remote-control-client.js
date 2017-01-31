2b695c5d3d2fb9a6e02df9d45a5c897e61c99b4e
matsub <matsub.rk@gmail.com>
2017-01-31 14:47:43 +0900

    add stopwatch

--- statics/custom/remote-control-client.js
+++ statics/custom/remote-control-client.js
@@ -1,3 +1,4 @@
+/* remote controls of reveal.js */
 var pubnub = PUBNUB({
     publish_key : '---INSERT KEY HERE---',
     subscribe_key : '---INSERT KEY HERE---',
@@ -7,8 +8,7 @@ var pubnub = PUBNUB({
 pubnub.subscribe({
     channel : 'output',
     message : function (message, envelope, channelOrGroup, time, channel) {
-        jQuery('#display').text(message.slide + '.' + message.part);
-        jQuery('#notes').html(message.notes);
+        jQuery('#note').html(message.notes);
     }
 });
 
@@ -19,12 +19,92 @@ function buttonCommand(button) {
     });
 }
 
+
+/* Stopwatch */
+class Stopwatch {
+    constructor(target) {
+        this.times;
+        this.running = false;
+        this.target = target;
+        this.reset();
+    }
+    
+    reset() {
+        this.times = [0, 0];
+        this.print(this.times);
+    }
+    
+    start() {
+        if (!this.time) this.time = performance.now();
+        if (!this.running) {
+            this.running = true;
+            requestAnimationFrame(this.step.bind(this));
+        }
+    }
+    
+    stop() {
+        this.running = false;
+        this.time = null;
+    }
+
+    step(timestamp) {
+        if (!this.running) return;
+        this.calculate(timestamp);
+        this.time = timestamp;
+        this.print();
+        requestAnimationFrame(this.step.bind(this));
+    }
+
+    calculate(timestamp) {
+        let diff = Math.abs(timestamp - this.time);
+        this.times[1] += diff / 1000;
+        if (this.times[1] >= 60) {
+            this.times[0] += 1;
+            this.times[1] -= 60;
+        }
+    }
+
+    print() {
+        let timeText = this.format(this.times);
+        this.target.text(timeText);
+    }
+
+    format(times) {
+        return times.map(Math.floor).map(pad0).join(':');
+    }
+}
+
+function pad0(value) {
+    let count = 2;
+    let result = value.toString();
+    for (; result.length < count; --count)
+        result = '0' + result;
+    return result;
+}
+
+function toggleSwitch() {
+    jQuery('#start').toggle();
+    jQuery('#stop').toggle();
+}
+
 jQuery(document).ready(function() {
-    jQuery('button').click(function (eventObject) {
-        var targetId = jQuery(this).attr('id');
+    /* remote controls of reveal.js */
+    jQuery('input').change((e) => { jQuery('.buttons').toggle(); });
+    jQuery('button').click((e) => {
+        let targetId = jQuery(e.currentTarget).attr('id');
         buttonCommand(targetId);
     });
-    jQuery('input').change(function (eventObject) {
-        jQuery('.buttons').toggle();
+
+    /* stopwatch */
+    let sw = new Stopwatch(jQuery('#stopwatch > time'));
+
+    jQuery('#start').click((e) => {
+        toggleSwitch();
+        sw.start();
     });
+    jQuery('#stop').click((e) => {
+        toggleSwitch();
+        sw.stop();
+    });
+    jQuery('#reset').click((e) => { sw.reset(); });
 });
